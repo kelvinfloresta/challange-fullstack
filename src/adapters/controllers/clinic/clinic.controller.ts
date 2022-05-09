47a46655714587a16bbafd45244cec5349fd5a8d
kelvinfloresta <kelvinf.contato@gmail.com>
2022-05-09 19:44:12 -0300

    feat: add clinic controller

---
+++ src/adapters/controllers/clinic/clinic.controller.ts
@@ -0,0 +1,19 @@
+
+import { Request, Response } from 'express'
+import { injectable } from 'tsyringe'
+import { ClinicCreateInput } from '../../../usecases/clinic/clinic.interface'
+import { ClinicCase } from '../../../usecases/clinic/clinic.usecase'
+
+@injectable()
+export class ClinicController {
+  constructor (private readonly clinicCase: ClinicCase) {}
+
+  async create (
+    req: Request<ClinicCreateInput>,
+    res: Response<string>
+  ) {
+    const id = await this.clinicCase.create(req.body)
+
+    res.status(201).send(id)
+  }
+}
