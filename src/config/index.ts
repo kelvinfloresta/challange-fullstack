0df8c4816c59e0b80b9c3cea68219fcda0128139
kelvinfloresta <kelvinf.contato@gmail.com>
2022-05-09 19:38:46 -0300

    feat(config): add database config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,12 @@
 import { requireConfig } from './require-config'
 
 export const config = {
-  port: requireConfig('PORT')
+  port: requireConfig('PORT'),
+  database: {
+    client: 'pg',
+    name: requireConfig('DATABASE_NAME'),
+    host: requireConfig('DATABASE_HOST'),
+    user: requireConfig('DATABASE_USER'),
+    password: requireConfig('DATABASE_PASSWORD')
+  }
 }
