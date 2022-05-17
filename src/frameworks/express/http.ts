b9523f72e097788cd6ee1c3c24c886bc5c02fdb7
kelvinfloresta <kelvinf.contato@gmail.com>
2022-05-17 18:45:26 -0300

    refactor(express): move http to express folder

--- src/frameworks/express/http.ts
+++ src/frameworks/express/http.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { DependencyContainer } from 'tsyringe'
-import { ClinicController } from '../adapters/controllers/clinic/clinic.controller'
-import { config } from '../config'
+import { ClinicController } from '../../adapters/controllers/clinic/clinic.controller'
+import { config } from '../../config'
+import { errorMiddleware } from './error.middleware'
+import { wrapAsync } from './wrap-async'
 
 export class ExpressHTTPServer {
   constructor (private readonly container: DependencyContainer) {}
@@ -10,7 +12,8 @@ export class ExpressHTTPServer {
     const clinic = this.container.resolve(ClinicController)
 
     express()
-      .post('/clinic', clinic.create)
+      .post('/clinic', wrapAsync(clinic.create))
+      .use(errorMiddleware)
       .listen(config.port, () => console.log(`listening on port: ${config.port}`))
   }
 }
